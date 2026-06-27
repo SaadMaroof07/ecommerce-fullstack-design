3d74793376e66763b901ab896bba112c03f48b59
Saad Maroof <saadmaroof07@gmail.com>
2026-06-27 17:46:50 +0500

    chore: auto create superuser

--- backend/ecommerce_backend/urls.py
+++ backend/ecommerce_backend/urls.py
@@ -12,3 +12,17 @@ urlpatterns = [
     path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+
+from django.contrib.auth import get_user_model
+from django.db.utils import IntegrityError
+
+User = get_user_model()
+
+try:
+   
+    User.objects.create_superuser('admin', 'saadmaroof07@gmail.com', 'admin1234')
+    print("Superuser created successfully via urls.py!")
+except IntegrityError:
+    
+    pass
